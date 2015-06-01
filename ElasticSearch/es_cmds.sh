1edc24271f900baaa3aac730499ae14e5f7eaea5
Ning Zou <ning.zou@rackspace.com>
2015-06-01 10:43:43 -0500

    added some queries

--- ElasticSearch/es_cmds.sh
+++ ElasticSearch/es_cmds.sh
@@ -21,33 +21,84 @@
 # 020-1234567
 # '
 
-# curl -X GET "http://192.168.33.10:9200/audit/_analyze?pretty=true&field=event.eventtime&text=2015-02-01T20:52:57.04+00:00"
+# curl -X GET "http://192.168.33.10:9200/audit/_analyze?pretty=true\
+# &field="event.target.typeURI"\
+# &text="service/bss/billing/Account""
 
+###### analyze: standard simple whitespace stop keyword -> these can be configured in settings
+###### https://www.elastic.co/guide/en/elasticsearch/reference/current/analysis-lang-analyzer.html
+###### whitespace, keyword is one without lower case tokenizer
+###### use keyword? simply mark the field as `not_analyzed`
+
+# curl -X GET "http://192.168.33.10:9200/audit/_analyze?pretty=true\
+# &analyzer=snowball\
+# &text=service/bss/billing/Account"
+
+# curl -X GET http://192.168.33.10:9200/audit/_analyze?analyzer=keyword&pretty=true -d '
+# service/bss/billing/account
+# '
 
 
 #### search
 
-# curl -X GET http://192.168.33.10:9200/audit/billing/_search?pretty -d '
+# curl -X GET http://192.168.33.10:9200/audit/billing/_search?pretty=true -d '
 # {
-#     "query": {
-#         "filtered": {
-#             "query": {
-#                 "match_all": {}
+#     "query" : {
+#         "filtered" : { 
+#             "query" : {
+#                 "match_all" : {} 
 #             },
-#             "filter": {
-#                 "and": [
+#             "filter" : {
+#                 "term" : { 
+#                     "event.target.typeURI" : "service"
+#                 }
+#             }
+#         }
+#     }
+# }
+# '
+
+# curl -X GET http://192.168.33.10:9200/audit/billing/_search?pretty=true -d '
+# {
+#     "query" : {
+#         "filtered" : { 
+#             "query" : {
+#                 "match_all" : {} 
+#             },
+#             "filter" : {
+#                 "bool" : {
+#                     "must" : {
+#                         "term" : { "event.target.typeURI" : "bss" }
+#                     },
+#                     "must_not" : {
+#                         "term" : { "event.target.typeURI" : "account" }
+#                     }
+#                 }
+#             }
+#         }
+#     }
+# }
+# '
+
+# curl -X GET http://192.168.33.10:9200/audit/billing/_search?pretty=true -d '
+# {
+#     "query" : {
+#         "filtered" : { 
+#             "query" : {
+#                 "match_all" : {} 
+#             },
+#             "filter" : {
+#                 "and" : [
 #                     {
 #                         "range" : {
-#                             "event.eventtime" : { 
-#                                 "from" : 2015-02-02, 
-#                                 "to" : "2015-02-03"
+#                             "event.eventTime" : {
+#                                 "from" : "2015-03-15",
+#                                 "to" : "2015-03-25"
 #                             }
-#                         },
+#                         }
 #                     },
 #                     {
-#                         "term": {
-#                             "event.target.id": "020-1234567"
-#                         }
+#                         "term" : { "event.target.typeURI" : "account" }
 #                     }
 #                 ]
 #             }
